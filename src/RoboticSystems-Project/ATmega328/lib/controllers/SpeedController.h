fdf86e49be189018db271f1b0076f4da40840c67
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-08-02 15:32:00 +0200

    fix: SpeedController_H -> SPEEDCONTROLLER_H.

--- src/RoboticSystems-Project/ATmega328/lib/controllers/SpeedController.h
+++ src/RoboticSystems-Project/ATmega328/lib/controllers/SpeedController.h
@@ -13,8 +13,8 @@
  * 	- is defined as right rotation.
  */
 
-#ifndef SpeedController_H
-#define SpeedController_H
+#ifndef SPEEDCONTROLLER_H
+#define SPEEDCONTROLLER_H
 
 #include <Arduino.h>
 
@@ -47,22 +47,8 @@ class SpeedController{
 		//target_linear_speed/target_vleft, target_angular_speed/target_vright, (false = lin/ang, true = vleft/vright).
 		void evaluate(float, float, bool = false);
 
-		float getLeftSpeed()							{	return enc.getLeftSpeed();	}
-		float getRightSpeed()							{	return enc.getRightSpeed();	}
-
-		float getLinearSpeed()						{	return enc.getLinearSpeed();	}
-		float getAngularSpeed()						{	return enc.getAngularSpeed();	}
-
-		//Cartesian.
-		float getX()											{	return enc.getX();	}
-		float getY()											{	return enc.getY();	}
-
-		//Polar.
-		float getRho()										{ return enc.getRho();			}
-		float getTheta(bool deg = false)	{ return enc.getTheta(deg);	}
-
-		int16_t getPWM_l()								{	return PWM_l;	}
-		int16_t getPWM_r()								{	return PWM_r;	}
+		int16_t getPWM_l()	{	return PWM_l;	}
+		int16_t getPWM_r()	{	return PWM_r;	}
 };
 
 #endif
