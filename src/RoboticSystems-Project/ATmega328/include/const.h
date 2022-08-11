81ed3574e841219c811554d54f9d444681a84b0a
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-08-11 20:57:37 +0200

    feat: added PID default values.

--- src/RoboticSystems-Project/ATmega328/include/const.h
+++ src/RoboticSystems-Project/ATmega328/include/const.h
@@ -1,3 +1,4 @@
+//PIN.
 #define ODOMETRY_SWITCH	7
 #define RESERVED_PIN		8		//Unused GPIO.
 
@@ -10,14 +11,23 @@
 #define LEFT_DIRECTION	4
 #define RIGHT_DIRECTION	5
 
-//NeoSWSerial
-#define SS_RX	A0
-#define SS_TX	A1
+//NeoSWSerial.
+#define SS_RX			A0
+#define SS_TX			A1
+#define SS_SPEED	9600
 
-//Sampling rate (8ms -> 125Hz)
+//Control system sampling rate (8ms -> 125Hz).
 #define DELTA_T	8e-3
 
-//RI32 wheel parameters.
+//Speed controller PID default values.
+#define S_KP	1000
+#define S_KI	3000
+
+//Position controller PID default values.
+#define P_MODULE_KP	1
+#define P_PHASE_KP	1
+
+//Encoder wheel parameters.
 #define ENC_RADIUS			0.021
 #define ENC_TICKS				1000
 #define ENC_WHEELBASE		0.305
