4c7567f4e0c1bbd1a17e5ee1e4778951e270623e
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-08-16 16:05:32 +0200

    feat: robot controlled by WebSerial ESP32.

--- src/RoboticSystems-Project/ATmega328/src/main.cpp
+++ src/RoboticSystems-Project/ATmega328/src/main.cpp
@@ -15,10 +15,6 @@
 //Number of plotted samples per second (debug).
 #define N_SAMPLES	30
 
-//Tollerance errors definition.
-#define TOL_RHO		0.01	//m.
-#define TOL_THETA	1			//deg.
-
 inline void handle_packet(), reset_routine(), start_timer2(), stop_timer2();
 
 //Serial plotter.
@@ -36,15 +32,19 @@ RI32 enc(LEFT_ENCODER_A, LEFT_ENCODER_B, RIGHT_ENCODER_A, RIGHT_ENCODER_B, DELTA
 SpeedController speedController(DELTA_T, S_KP, S_KI, LMD18200_PWM_MAX_VAL, motor, enc);
 PositionController positionController(DELTA_T, P_MODULE_KP, P_PHASE_KP, ROB_MAX_SPEED, speedController);
 
-//Target for the position controller.
+// Target for the position controller.
 float	target_x = 0, target_y = 0, target_theta = 0;		//m, m, deg.
-float target_rho = 0;																	//hypot(target_x, target_y);
+float target_rho = hypot(target_x, target_y);					//m.
+
+//Tollerance errors definition.
+#define TOL_RHO		0.1	//m.
+#define TOL_THETA	5		//deg.
 
 //Tick flag.
 volatile bool tick = false;
 
-//It needs to lock the control system when the target is reached.
-bool evaluate = true;
+//Needed to lock the control system when the target is reached.
+bool evaluate = false;
 
 //Tick counter (used for debug pourposes).
 int c = 0;
@@ -87,47 +87,36 @@ void loop(){
 
 		if(c++ == int(1 / (N_SAMPLES * DELTA_T))){
 			c = 0;
-
 			plotter.start();
+			plotter.add(freeMemory());
 			
-			plotter.add(enc.getX() * 1000);
-			plotter.add(enc.getY() * 1000);
 			plotter.add(enc.getRho() * 1000);
 			plotter.add(enc.getTheta(true));
 
 			plotter.add(target_rho * 1000);
 			plotter.add(target_theta);
-			
-			plotter.add(speedController.getLeftPWM());
-			plotter.add(speedController.getRightPWM());
 
+			plotter.add(enc.getX() * 1000);
+			plotter.add(enc.getY() * 1000);
+			
 			plotter.plot();
 		}
 	}
 
 	//Time to check for some serial packets.
-	else{
-		if(ss.available()){
-			ss.readBytes((uint8_t*) &packet, sizeof(packet));
-			handle_packet();
-			ss.write((uint8_t*) &packet, sizeof(packet));
-		}
+	if(ss.available()){
+		ss.readBytes((uint8_t*) &packet, sizeof(packet));
+		handle_packet();
+		ss.write((uint8_t*) &packet, sizeof(packet));
 	}
 }
 
 inline void handle_packet(){
-	//Check for CRC8 errors.
-	if(crc8_packet<>(packet) != packet.crc8){
-		packet.payload.msg = CONTROL_WRONG_CRC;
-		packet.payload.argc = 0;
-		return;
-	}
-
-	packet_command_t com = packet.payload.msg;
+	packet_command_t com = packet.com;
 
 	//Default reply values.
-	packet.payload.msg = CONTROL_OK;
-	packet.payload.argc = 0;
+	packet.com = CONTROL_OK;
+	packet.argc = 0;
 
 	switch(com){		
 		case COMMAND_RESET:
@@ -139,47 +128,46 @@ inline void handle_packet(){
 			break;
 		
 		case COMMAND_POSE:
-			packet.payload.argc = 3;
+			packet.argc = 3;
 
-			packet.payload.argv[0] = enc.getX();
-			packet.payload.argv[1] = enc.getY();
-			packet.payload.argv[2] = enc.getTheta();
+			packet.argv[0] = enc.getX();
+			packet.argv[1] = enc.getY();
+			packet.argv[2] = enc.getTheta();
 			break;
 		
 		case COMMAND_GOTO:
-			target_x = packet.payload.argv[0];
-			target_y = packet.payload.argv[1];
-			target_theta = packet.payload.argv[2];
+			target_x = packet.argv[0];
+			target_y = packet.argv[1];
+			target_theta = packet.argv[2];
 			target_rho = hypot(target_x, target_y);
+
+			evaluate = true;
 			break;
 		
 		case COMMAND_KPID_SET:
-			positionController.setModuleKp(packet.payload.argv[0]);
-			positionController.setPhaseKp(packet.payload.argv[1]);
-			speedController.setKp(packet.payload.argv[2]);
-			speedController.setKi(packet.payload.argv[3]);
+			positionController.setModuleKp(packet.argv[0]);
+			positionController.setPhaseKp(packet.argv[1]);
+			speedController.setKp(packet.argv[2]);
+			speedController.setKi(packet.argv[3]);
 		
 		//Continue after COMMAND_KPID_SET:
 		case COMMAND_KPID_GET:
-			packet.payload.argc = 4;
+			packet.argc = 4;
 
-			packet.payload.argv[0] = positionController.getModuleKp();
-			packet.payload.argv[1] = positionController.getPhaseKp();
-			packet.payload.argv[2] = speedController.getKp();
-			packet.payload.argv[3] = speedController.getKi();
+			packet.argv[0] = positionController.getModuleKp();
+			packet.argv[1] = positionController.getPhaseKp();
+			packet.argv[2] = speedController.getKp();
+			packet.argv[3] = speedController.getKi();
 			break;
 		
 		case CONTROL_OK:
-		case CONTROL_WRONG_CRC:
 		case CONTROL_INVALID_MSG:
 			break;
 
 		default:
-			packet.payload.msg = CONTROL_INVALID_MSG;
+			packet.com = CONTROL_INVALID_MSG;
 			break;
 	}
-
-	packet.crc8 = crc8_packet<>(packet);
 }
 
 inline void reset_routine(){
