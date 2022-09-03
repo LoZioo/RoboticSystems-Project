eca14af589d6254cd59bb4e21f1721089618e0c5
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-09-03 13:51:47 +0200

    feat: updated code.

--- src/RoboticSystems-Project/ESP32/src/main.cpp
+++ src/RoboticSystems-Project/ESP32/src/main.cpp
@@ -16,11 +16,14 @@
 inline void settings_save(), settings_load();
 
 //Thread.
-void webserver_thread(void*), evaluate_thread(void*);
-TaskHandle_t webserver_thread_handle, evaluate_thread_handle;
+void webserver_thread(void*);
+TaskHandle_t webserver_thread_handle;
 
 //WebServer.
 AsyncWebServer server(80);
+
+//Old positions.
+float old_x = 0, old_y = 0;
 
 //Fixed graph.
 FixedGraph graph;
@@ -31,7 +34,7 @@ settings_t settings;
 void setup(){
 	Serial.begin(9600);
 	Serial.setDebugOutput(false);
-	
+
 	LittleFS.begin();
 	settings_load();
 
@@ -51,8 +54,7 @@ void setup(){
 	}
 
 	//Main taks spawn.
-	xTaskCreatePinnedToCore(webserver_thread,	"webserver",	10240,	NULL,	1,	&webserver_thread_handle,	PRO_CPU);
-	// xTaskCreatePinnedToCore(evaluate_thread,	"evaluate",		10240,	NULL,	1,	&evaluate_thread_handle,	APP_CPU);
+	xTaskCreatePinnedToCore(webserver_thread,	"webserver",	10240,	NULL,	1,	&webserver_thread_handle,	APP_CPU);
 
 	//Delete setup task.
 	vTaskDelete(NULL);
@@ -61,51 +63,6 @@ void setup(){
 void loop(){}
 
 void webserver_thread(void *parameters){
-	server.on("/test", HTTP_GET, [](AsyncWebServerRequest *req){
-		graph.clearVertex(0.2, 0);
-		graph.dijkstra(0, 0);
-
-		std::pair<float, float> shortest_path[FIXED_GRAPH_SIZE];
-		int len;
-		
-		graph.getShortestPath(1, 0, shortest_path, len);
-		len--;	//There are len - 1 elements; (0, 0) isn't an element!
-
-		int j = 0;
-		xy_t xy[len];
-
-		//Exclude the source.
-		for(int i=len-1; i>=0; i--){
-			xy[j].x = shortest_path[i].first;
-			xy[j].y = shortest_path[i].second;
-
-			j++;
-		}
-
-		//Sending data.
-		packet_t<> packet;
-
-		packet.com = COMMAND_WAIT_XY_ARRAY;
-		packet.argc = 1;
-		packet.argv[0] = len;
-
-		Serial.write((uint8_t*) &PACKET_PAYLOAD, sizeof(PACKET_PAYLOAD));
-		Serial.write((uint8_t*) &packet, sizeof(packet));
-		Serial.write((uint8_t*) &xy, sizeof(xy));
-
-		DynamicJsonDocument doc(1024);
-		String json;
-		
-		for(int i=0; i<len; i++){
-			doc["data"][i]["x"] = xy[i].x;
-			doc["data"][i]["y"] = xy[i].y;
-		}
-
-		serializeJson(doc, json);
-		req->send(200, "application/json", json);
-	});
-	///////////////////////////////////////////////////////////////////////
-
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *req){
 		req->redirect("/index.html");
 	});
@@ -131,54 +88,116 @@ void webserver_thread(void *parameters){
 
 	server.on("/commands/general", HTTP_POST, [](AsyncWebServerRequest *req){
 		//Example:	{"com":10,"data":[1,1,1,1]}
+
+		//Read JSON from request.
 		DynamicJsonDocument doc(1024);
-		
 		deserializeJson(doc, req->arg("data"));
 
+		//Build packet.
+		packet_t<> packet;
+
+		packet.com = doc["com"];
+		packet.argc = 4;
+		
+		for(int i=0; i<packet.argc; i++)
+			packet.argv[i] = doc["args"][i];
+		
+		//Flush incoming bytes.
+		while(Serial.available())
+			Serial.read();
+		
+		//Send request.
+		Serial.write((uint8_t*) &PACKET_PAYLOAD, sizeof(PACKET_PAYLOAD));
+		Serial.write((uint8_t*) &packet, sizeof(packet));
+
+		//Read response (no payload check required).
+		Serial.readBytes((uint8_t*) &packet, sizeof(packet));
+
+		//Encode response.
+		doc.clear();
+		doc["com"] = packet.com;
+
+		//Set parameters.
+		for(int i=0; i<packet.argc; i++)
+			doc["args"][i] = packet.argv[i];
+		
+		//Fill with zeros.
+		for(int i=packet.argc; i<PACKET_ARGV_MAXLEN; i++)
+			doc["args"][i] = 0;
+
+		//Encode JSON for the response.
+		String res;
+		serializeJson(doc, res);
+
+		//Send response.
+		req->send(200, "application/json", res);
+	});
+
+	server.on("/commands/obstacle/add", HTTP_POST, [](AsyncWebServerRequest *req){
+		const float x = req->arg("x").toFloat();
+		const float y = req->arg("y").toFloat();
+
+		graph.clearVertex(x, y);
+		req->send(200);
+	});
+
+	server.on("/commands/obstacle/remove", HTTP_POST, [](AsyncWebServerRequest *req){
+		const float x = req->arg("x").toFloat();
+		const float y = req->arg("y").toFloat();
+
+		graph.setVertex(x, y);
+		req->send(200);
+	});
+
+	server.on("/commands/exec_route", HTTP_POST, [](AsyncWebServerRequest *req){
+		const float x = req->arg("x").toFloat();	//Destination x.
+		const float y = req->arg("y").toFloat();	//Destination y.
+
+		//Update shortest path.
+		graph.dijkstra(old_x, old_y);
+
+		//Update source point for the next exec_route.
+		old_x = x;
+		old_y = y;
+
+		//Get shortest path.
+		std::pair<float, float> shortest_path[FIXED_GRAPH_SIZE];
+		int len;
+
+		graph.getShortestPath(x, y, shortest_path, len);
+		len--;	//There are len - 1 elements; (0, 0) isn't an element!
+
+		//Pack points array.
+		int j = 0;
+		xy_t xy[len];
+
+		//Debug: also send the result points to the client.
+		DynamicJsonDocument doc(1024);
+
+		//len-1: exclude the source.
+		for(int i=len-1; i>=0; i--){
+			doc["points"][j]["x"] = xy[j].x = shortest_path[i].first;
+			doc["points"][j]["y"] = xy[j].y = shortest_path[i].second;
+
+			j++;
+		}
+
+		//Send data.
+		packet_t<> packet;
+
+		packet.com = COMMAND_WAIT_XY_ARRAY;
+		packet.argc = 1;
+		packet.argv[0] = len;
+
+		Serial.write((uint8_t*) &PACKET_PAYLOAD, sizeof(PACKET_PAYLOAD));
+		Serial.write((uint8_t*) &packet, sizeof(packet));
+		Serial.write((uint8_t*) &xy, sizeof(xy));
+
+		//Send response.
 		String res;
 		serializeJson(doc, res);
 
 		req->send(200, "application/json", res);
-
-		//CONTINUARE IMPLEMENTANDO GLI ENDPOINT RIMANENTI
-
-		// deserializeJson(doc, data);
-
-		// //Set parameters number.
-		// packet.argc = doc["data"].size();
-
-		// //Set parameters.
-		// for(int i=0; i<packet.argc; i++)
-		// 	packet.argv[i] = doc["data"][i];
-
-		// //Flush incoming bytes.
-		// while(Serial.available())
-		// 	Serial.read();
-		
-		// //Send request.
-		// Serial.write((uint8_t*) &packet, sizeof(packet));
-
-		// //Wait response.
-		// while(!Serial.available())
-		// 	asm("nop");
-		
-		// //Read response.
-		// Serial.readBytes((uint8_t*) &packet, sizeof(packet));
-
-		// //Encode response.
-		// doc.clear();
-		// doc["com"] = packet.com;
-
-		// //Set parameters.
-		// for(int i=0; i<packet.argc; i++)
-		// 	doc["data"][i] = packet.argv[i];
-		
-		// serializeJson(doc, res);
-
-		// //Send JSON.
-		// WebSerial.println(res);
-
-		// req->send(200, "text/plain", settings.ssid);
 	});
 
 	server.serveStatic("/settings.bin", LittleFS, "/settings.bin");
@@ -189,11 +208,6 @@ void webserver_thread(void *parameters){
 
 	for(;;)
 		vTaskDelay(1 / portTICK_PERIOD_MS);		//One ms delay.
-}
-
-void evaluate_thread(void *parameters){
-	for(;;)
-		vTaskDelay(1 / portTICK_PERIOD_MS);
 }
 
 inline void settings_save(){
